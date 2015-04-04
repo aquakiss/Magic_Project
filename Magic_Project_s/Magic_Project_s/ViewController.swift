eb54f0a6773642269e3916ea2df9146fd3befa5c
Noupi <developer@Mac-de-Developer.local>
2015-04-04 15:17:05 +0200

    add project swift

---
+++ Magic_Project_s/Magic_Project_s/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magic_Project_s
+//
+//  Created by Developer on 04/04/2015.
+//  Copyright (c) 2015 Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
